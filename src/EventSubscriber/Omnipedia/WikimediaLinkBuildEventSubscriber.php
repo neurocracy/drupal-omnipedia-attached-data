8f95b55bbfe237d9a4a4ef47eed3df6e33a0d108
Matei Stanca <i@ambientimpact.com>
2020-11-07 15:18:59 -0500

    WikimediaLink attached data plug-in and event subscriber.

---
+++ src/EventSubscriber/Omnipedia/WikimediaLinkBuildEventSubscriber.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Drupal\omnipedia_attached_data\EventSubscriber\Omnipedia;
+
+use Drupal\Component\Utility\Html;
+use Drupal\omnipedia_attached_data\OmnipediaAttachedDataManagerInterface;
+use Drupal\omnipedia_content\Event\Omnipedia\WikimediaLinkBuildEvent;
+use Drupal\omnipedia_content\OmnipediaContentEventInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * Event subscriber to attach data to matching Wikimedia links.
+ */
+class WikimediaLinkBuildEventSubscriber implements EventSubscriberInterface{
+
+  /**
+   * The OmnipediaAttachedData plug-in manager.
+   *
+   * @var \Drupal\omnipedia_attached_data\OmnipediaAttachedDataManagerInterface
+   */
+  protected $attachedDataManager;
+
+  /**
+   * Event subscriber constructor; saves dependencies.
+   *
+   * @param \Drupal\omnipedia_attached_data\OmnipediaAttachedDataManagerInterface $attachedDataManager
+   *   The OmnipediaAttachedData plug-in manager.
+   */
+  public function __construct(
+    OmnipediaAttachedDataManagerInterface $attachedDataManager
+  ) {
+    $this->attachedDataManager = $attachedDataManager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents(): array {
+    return [
+      OmnipediaContentEventInterface::WIKIMEDIA_LINK_BUILD => 'onWikimediaLinkBuild',
+    ];
+  }
+
+  /**
+   * WikimediaLinkBuildEvent callback.
+   *
+   * @param \Drupal\omnipedia_content\Event\Omnipedia\WikimediaLinkBuildEvent $event
+   *   The event object.
+   *
+   * @todo Find way of getting the date of the content we're rendering in, so
+   *   that we don't have to use 'current' as the date passed to
+   *   OmnipediaAttachedDataManagerInterface::getContent().
+   */
+  public function onWikimediaLinkBuild(WikimediaLinkBuildEvent $event): void {
+    /** @var \League\CommonMark\Inline\Element\Link */
+    $link = $event->getLink();
+
+    /** @var string|null */
+    $attachedDataContent = $this->attachedDataManager->getContent(
+      'wikimedia_link', $event->getPrefixedUrl(), 'current'
+    );
+
+    if (!empty($attachedDataContent)) {
+      $link->data['attributes']['data-omnipedia-attached-data-content'] =
+        Html::escape($attachedDataContent);
+
+      // Save a copy of the attached data content with tags stripped to the
+      // title attribute as a fallback if our JavaScript fails for whatever
+      // reason.
+      $link->data['attributes']['title'] =
+        Html::escape(\strip_tags($attachedDataContent));
+    }
+  }
+
+}
---
+++ src/Plugin/Omnipedia/AttachedData/WikimediaLink.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Drupal\omnipedia_attached_data\Plugin\Omnipedia\AttachedData;
+
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Render\RendererInterface;
+use Drupal\Core\StringTranslation\TranslationInterface;
+use Drupal\omnipedia_attached_data\OmnipediaAttachedDataBase;
+use Drupal\omnipedia_content\Service\WikimediaLinkInterface;
+use Drupal\omnipedia_core\Service\TimelineInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Wikimedia link attached data plug-in.
+ *
+ * @OmnipediaAttachedData(
+ *   id           = "wikimedia_link",
+ *   title        = @Translation("Wikimedia link"),
+ *   description  = @Translation("A Wikimedia prefixed link pointing to an article on one of the Wikimedia sites, e.g. <code>wikipedia:Owl</code> will point to the Wikipedia article about Owls.")
+ * )
+ */
+class WikimediaLink extends OmnipediaAttachedDataBase {
+
+  /**
+   * The Omnipedia Wikimedia link service.
+   *
+   * @var \Drupal\omnipedia_content\Service\WikimediaLinkInterface
+   */
+  protected $wikimediaLink;
+
+  /**
+   * {@inheritdoc}
+   *
+   * @param \Drupal\omnipedia_content\Service\WikimediaLinkInterface $wikimediaLink
+   *   The Omnipedia Wikimedia link service.
+   */
+  public function __construct(
+    array $configuration, string $pluginId, array $pluginDefinition,
+    RendererInterface           $renderer,
+    TranslationInterface        $stringTranslation,
+    TimelineInterface           $timeline,
+    EntityTypeManagerInterface  $entityTypeManager,
+    WikimediaLinkInterface      $wikimediaLink
+  ) {
+    parent::__construct(
+      $configuration, $pluginId, $pluginDefinition,
+      $entityTypeManager, $renderer, $stringTranslation, $timeline
+    );
+
+    // Save dependencies.
+    $this->wikimediaLink      = $wikimediaLink;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(
+    ContainerInterface $container,
+    array $configuration, $pluginId, $pluginDefinition
+  ) {
+    return new static(
+      $configuration, $pluginId, $pluginDefinition,
+      $container->get('renderer'),
+      $container->get('string_translation'),
+      $container->get('omnipedia.timeline'),
+      $container->get('entity_type.manager'),
+      $container->get('omnipedia.wikimedia_link')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   *
+   * @todo List valid prefixes?
+   */
+  public function validateTarget(string $target): array {
+    $errors = [];
+
+    if (!$this->wikimediaLink->isPrefixUrl($target)) {
+      $errors[] = $this->t(
+        '"@target" does not begin with a valid Wikimedia link prefix.',
+        ['@target' => $target]
+      );
+    }
+
+    return $errors;
+  }
+
+}
