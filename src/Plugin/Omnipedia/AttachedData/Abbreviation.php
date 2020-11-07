f6f3990970b8e271cb1a33bc8e987d0b91a92811
Matei Stanca <i@ambientimpact.com>
2020-11-07 15:21:17 -0500

    Added skeleton Abbreviation attached data plug-in.

---
+++ src/Plugin/Omnipedia/AttachedData/Abbreviation.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\omnipedia_attached_data\Plugin\Omnipedia\AttachedData;
+
+use Drupal\omnipedia_attached_data\OmnipediaAttachedDataBase;
+
+/**
+ * Abbreviation attached data plug-in.
+ *
+ * @OmnipediaAttachedData(
+ *   id           = "abbreviation",
+ *   title        = @Translation("Abbreviation"),
+ *   description  = @Translation("An abbreviated term to match, e.g. <abbr title='HyperText Markup Language'>HTML</abbr>, <abbr title='Cascading Style Sheets'>CSS</abbr>, etc.")
+ * )
+ */
+class Abbreviation extends OmnipediaAttachedDataBase {
+}
