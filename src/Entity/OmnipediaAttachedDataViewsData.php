cd6d9df0718ba735b33624564c46c07d963087fa
Matei Stanca <i@ambientimpact.com>
2020-11-13 22:01:38 -0500

    Added attached data type Views filter plug-in.

--- src/Entity/OmnipediaAttachedDataViewsData.php
+++ src/Entity/OmnipediaAttachedDataViewsData.php
@@ -16,12 +16,29 @@ class OmnipediaAttachedDataViewsData extends EntityViewsData {
     /** @var array */
     $data = parent::getViewsData();
 
+    /** @var \Drupal\Core\StringTranslation\TranslatableMarkup */
+    $groupName = $this->t('Omnipedia: Attached data');
+
     $data['omnipedia_attached_data']['omnipedia_attached_data_bulk_form'] = [
-      'title' => $this->t('Omnipedia attached data operations bulk form'),
+      'title' => $this->t('Bulk operations form'),
+      'group' => $groupName,
       'help'  => $this->t('Add a form element that lets you run operations on multiple attached data.'),
       'field' => [
         'id'    => 'omnipedia_attached_data_bulk_form',
       ],
+    ];
+
+    $data['omnipedia_attached_data']['type']['group'] = $groupName;
+
+    $data['omnipedia_attached_data']['type']['filter'] = [
+      'title'   => $this->t('Type'),
+      // This is the help text shown on the plug-in options modal in the Views
+      // UI.
+      'help'    => $this->t('Filter by attached data type.'),
+      // This is the column in the table that this plug-in operates on.
+      'field'   => 'type',
+      // This is the @ViewsFilter() annotation value of our plug-in.
+      'id'      => 'omnipedia_attached_data_type',
     ];
 
     return $data;
---
+++ src/Plugin/views/filter/OmnipediaAttachedDataTypeFilter.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Drupal\omnipedia_attached_data\Plugin\views\filter;
+
+use Drupal\omnipedia_attached_data\OmnipediaAttachedDataManagerInterface;
+use Drupal\views\Plugin\views\display\DisplayPluginBase;
+use Drupal\views\Plugin\views\filter\InOperator;
+use Drupal\views\ViewExecutable;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Filter to handle Omnipedia attached data types.
+ *
+ * @ingroup views_filter_handlers
+ *
+ * @ViewsFilter("omnipedia_attached_data_type")
+ */
+class OmnipediaAttachedDataTypeFilter extends InOperator {
+
+  /**
+   * The OmnipediaAttachedData plug-in manager.
+   *
+   * @var \Drupal\omnipedia_attached_data\OmnipediaAttachedDataManagerInterface
+   */
+  protected $attachedDataManager;
+
+  /**
+   * Constructs an OmnipediaAttachedDataTypeFilter object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plug-in instance.
+   *
+   * @param string $pluginId
+   *   The plug-in ID for the plug-in instance.
+   *
+   * @param mixed $pluginDefinition
+   *   The plug-in implementation definition.
+   *
+   * @param \Drupal\omnipedia_attached_data\OmnipediaAttachedDataManagerInterface $attachedDataManager
+   *   The OmnipediaAttachedData plug-in manager.
+   */
+  public function __construct(
+    array $configuration, $pluginId, $pluginDefinition,
+    OmnipediaAttachedDataManagerInterface $attachedDataManager
+  ) {
+    parent::__construct($configuration, $pluginId, $pluginDefinition);
+
+    // Save dependencies.
+    $this->attachedDataManager = $attachedDataManager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(
+    ContainerInterface $container,
+    array $configuration,
+    $pluginId,
+    $pluginDefinition
+  ) {
+    return new static(
+      $configuration,
+      $pluginId,
+      $pluginDefinition,
+      $container->get('plugin.manager.omnipedia_attached_data')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function init(
+    ViewExecutable $view, DisplayPluginBase $display, array &$options = null
+  ) {
+    parent::init($view, $display, $options);
+
+    $this->definition['options callback'] = [
+      $this->attachedDataManager, 'getAttachedDataTypeOptionValues'
+    ];
+  }
+
+}
